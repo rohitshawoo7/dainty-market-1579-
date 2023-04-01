75fd504c70e9fcb469837d2636e8adc398835f25
Rohit shaw <rohitshawofficial1999@gmail.com>
2023-04-01 15:40:02 +0530

    signinpage

--- dailydeals2.js
+++ dailydeals2.js
@@ -1,3 +1,28 @@
-function(""){
-
-}
+const getData = async () => {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products?limit=5");
+      const data = await res.json();
+      console.log(data);
+      AppendData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getData();
+  
+  const AppendData = (data) => {
+    const container = document.querySelector(".containerImages");
+  
+    data.map((el) => {
+      const box = document.createElement("div");
+  
+      const img = document.createElement("img");
+      img.src = el.image;
+      const price = document.createElement("h1");
+      price.textContent = el.price;
+  
+      box.append(img, price);
+  
+      container.append(box);
+    });
+  };
